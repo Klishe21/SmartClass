31a4bcc6dd986696d8625b40c1becaa693063bd9
Pablo Querales <pabloquerales20@gmail.com>
2023-10-14 16:38:03 +0200

    se generó la función para crear cards automáticamente

--- main.js
+++ main.js
@@ -13,19 +13,40 @@ let alumnos = [{
 {
     name: "Santiago" ,
     price : 80
+    
+},
+{
+    name: "Camila" ,
+    price : 70
 }]
 
-const prev = document.querySelectorAll(".add-lesson span");
+const contenedorCards = document.querySelector("#contenedor-cards");
+
+
+// FUNCION ENCARGADA DE GENERAR CARDS DE ALUMNOS
 function cargarNombre() {
     alumnos.forEach( alumnos =>{
         const div = document.createElement("div")
-        div.classList.add("nombre")
+        div.classList.add("resumen-estudiantes-card")
         div.innerHTML = `
-            <h2>${alumnos.name}</h2>
-            <p>Precio X Hra : S/ ${alumnos.price}</p>
+            <div class="nombre">
+				<h2>${alumnos.name}</h2>
+				<p>Precio X Hra : S/ ${alumnos.price}</p>
+			</div>
+			<div class="resumen-clases">
+				<div class="resumen-estudiantes-card-actual">
+					<h3>Actual</h3>
+				</div>
+				<div class="resumen-estudiantes-card-acumulado">
+					<h3>Acumulado</h3>
+				</div>
+			</div>
+			<div class="add-lesson">
+				<h3>Agregar Clase</h3>
+				<span id="prev"> <i class="bi bi-plus-circle"></i></span>
+			</div>
         `
+        contenedorCards.append(div)
     })
 } 
-
-console.log(nombres)
-
+cargarNombre()
